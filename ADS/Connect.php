9379cef1a2ad8e7a2e2c36964aa9a6983d947e40
Elitrea <65686493+Elitrea@users.noreply.github.com>
2022-12-17 21:26:09 -0600

    conexión y menú

--- ADS/Connect.php
+++ ADS/Connect.php
@@ -1,5 +1,7 @@
 <?php
-        
         $conex=mysqli_connect("localhost","root","","ADSbd");
+        if(!$conex){
+                die("Error al conectarse a la base de datos: ".mysqli_connect_error());
+        }
         
 ?>
