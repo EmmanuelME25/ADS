accf5002980293c50d79f6e8e385a0e606d769ee
EmmanuelME25 <52678688+EmmanuelME25@users.noreply.github.com>
2022-12-18 20:21:56 -0600

    Add files via upload
    Funcionando

--- ADS/VALIDARINGRESO.php
+++ ADS/VALIDARINGRESO.php
@@ -3,25 +3,70 @@
 $usuario=$_POST['correo'];
 $contrasena=$_POST['clave'];
 
+
 session_start();
 $_SESSION['correo']=$usuario;
 
 include('Connect.php');
 
+
+
 $consulta="SELECT * FROM usuario WHERE correo= '$usuario' and clave ='$contrasena'";
-
 $resultado=mysqli_query($conex,$consulta);
-
+$datos=mysqli_fetch_array($resultado);
 $filas=mysqli_num_rows($resultado);
+
 
 if($filas)
 {
-
-header("location:inicio.html");
-
-}else{
-    header("location:index.php");
+    
+if($datos['correoAux']==NULL)
+{
+    header("location:02_inicio_primera_vez.html");
 }
+else 
+{
+
+$cp="SELECT * FROM padre WHERE usuario_correo= '$usuario'";
+$rcp=mysqli_query($conex,$cp);
+$fcp=mysqli_num_rows($rcp);
+if($fcp)
+{
+    header("location:padre/24_pagina_principal_general.html");
+}
+$ca="SELECT * FROM alumno WHERE usuario_correo= '$usuario'";
+$rca=mysqli_query($conex,$ca);
+$fca=mysqli_num_rows($rca);
+if($fca)
+{
+    header("location:alumno/24_pagina_principal_general.html");
+}
+$cad="SELECT * FROM administrador WHERE usuario_correo= '$usuario'";
+$rcad=mysqli_query($conex,$cad);
+$fcad=mysqli_num_rows($rcad);
+if($fcad)
+{
+    header("location:admin/15_pagina_principal_admin.html");
+}
+$cpr="SELECT * FROM profesor WHERE usuario_correo= '$usuario'";
+$rcpr=mysqli_query($conex,$cpr);
+$fcpr=mysqli_num_rows($rcpr);
+if($fcpr)
+{
+    header("location:docente/03_pagina_principal_docente.html");
+}
+
+}
+
+
+}
+else{
+
+    header("location:index.html");
+
+}
+
+
 mysqli_free_result($resultado);
 mysqli_close($conex);
 ?>
